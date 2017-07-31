7f97daafd76346677f78e3afb32d4dbe61667f59
Mark Katerberg <katerberg@fastmail.fm>
2017-07-31 12:47:16 -0500

    Initial Build

---
+++ src/EnterTextField.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+class EnterTextField extends React.Component {
+    constructor(props) {
+        super(props);
+        const {
+            onEnterKeyPress,
+            ...other
+        } = props;
+        this.onEnterKeyPress = onEnterKeyPress;
+        this.other = other;
+    }
+
+    handleKeyPress(event) {
+        if (event.charCode === 13) {
+            this.onEnterKeyPress();
+        }
+    }
+
+    render() {
+        return (
+            <TextField onKeyPress={e => this.handleKeyPress(e)}
+                {...this.other}/>
+        );
+    }
+}
+
+export default EnterTextField;
+
---
+++ src/EnterTextFieldSpec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import chai, { expect } from 'chai';
+import sinon, { stub } from 'sinon';
+import EnterTextField from './EnterTextField';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+describe('EnterTextField', () => {
+
+    let testObject;
+    let onEnterKeyPress;
+
+    beforeEach(() => {
+        onEnterKeyPress = sinon.stub();
+        testObject = new EnterTextField({onEnterKeyPress});
+    });
+
+    describe('#handleKeyPress()', () => {
+        it('ignores most key presses', () => {
+            testObject.handleKeyPress({charCode: 20});
+            testObject.handleKeyPress({charCode: 22});
+            testObject.handleKeyPress({charCode: 130});
+            testObject.handleKeyPress({charCode: 10});
+
+            expect(onEnterKeyPress).not.to.have.been.called;
+        });
+
+        it('fires event when enter key is pressed', () => {
+            testObject.handleKeyPress({charCode: 13});
+
+            expect(onEnterKeyPress).to.have.been.called;
+        });
+    });
+});
+
